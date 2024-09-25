f9cea9cf4fd37ecaa4a56daeb47cf06ca0289d24
Avril Maleham <avrilmaleham@outlook.com>
2024-09-25 16:06:47 +1200

    feat: ref vs state vs variable demo

--- src/app/useRef/page.tsx
+++ src/app/useRef/page.tsx
@@ -1,10 +1,43 @@
 "use client";
+import { useRef, useState } from "react";
 import BackButton from "@/components/backButton";
 
 export default function useRefDemo() {
+
+  /* Ref vs State vs Variable Demo */
+  // Any variables rendered inside the returned JSX should be
+  // part of STATE, so that updates trigger re-rendering and keep
+  // everything in sync. Updates to refs and normal variables DO NOT
+  // trigger re-renders, so the updated count values don’t show.
+
+  const [countState, setCountState] = useState(0); // one counter stored in state
+
+  const countRef = useRef(0); // one counter stored in a ref
+  let count = 0; // one counter stored in a normal variable
+
+  function handleClick() {
+    // update countRef object when clicking, via current property
+    countRef.current = countRef.current + 1;
+    count = count + 1; // update count variable when clicking
+    // both counts should be the same value
+    alert(`You clicked ${countRef.current} (${count}) times!`);
+  }
+
+
   return (
     <div className="flex flex-col items-center justify-center h-screen font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-4xl font-bold m-5">useRef Demo</h1>
+
+      <div className="flex flex-col items-center justify-center">
+        <h2 className="text-2xl font-bold m-4">Ref vs State vs Variable Demo</h2>
+        <button onClick={handleClick}>REF COUNTER: Click me!</button> Ref:{" "}
+        {countRef.current} Var: {count} <br />
+        <button onClick={() => setCountState(countState + 1)}>
+          STATE COUNTER: Click me to update!
+        </button>{" "}
+        State: {countState}
+      </div>
+
       <BackButton />
     </div>
   );
